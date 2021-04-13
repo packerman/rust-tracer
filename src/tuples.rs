1e8f5dd2d1ccb0e7ee875ef1ca17ad58be7e1acf
Piotr Ackermann <pioacker@gmail.com>
2021-04-13 19:40:15 +0200

    Tuples - normalizing

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,3 +1,4 @@
+use approx::AbsDiffEq;
 use std::ops;
 
 #[derive(PartialEq, Debug)]
@@ -24,6 +25,15 @@ impl Tuple {
 
     fn is_vector(&self) -> bool {
         self.w == 0.0
+    }
+
+    fn magnitude(&self) -> f32 {
+        (self.x*self.x + self.y*self.y + self.z*self.z + self.w*self.w).sqrt()
+    }
+
+    fn normalize(&self) -> Tuple {
+        let m = self.magnitude();
+        Tuple::new(self.x / m, self.y / m, self.z / m, self.w / m)
     }
 }
 
@@ -53,10 +63,54 @@ impl ops::Sub for Tuple {
      }
 }
 
+impl ops::Neg for Tuple {
+
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        Tuple::new(-self.x, -self.y, -self.z, -self.w)
+     }
+}
+
+impl ops::Mul<f32> for Tuple {
+
+    type Output = Self;
+
+    fn mul(self, factor: f32) -> Self {
+        Tuple::new(self.x * factor, self.y * factor, self.z * factor, self.w * factor)
+     }
+}
+
+impl ops::Div<f32> for Tuple {
+
+    type Output = Self;
+
+    fn div(self, factor: f32) -> Self {
+        Tuple::new(self.x / factor, self.y / factor, self.z / factor, self.w / factor)
+     }
+}
+
+impl AbsDiffEq for Tuple {
+
+    type Epsilon = f32;
+
+    fn default_epsilon() -> Self::Epsilon {
+        f32::default_epsilon()
+     }
+
+    fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        self.x.abs_diff_eq(&other.x, epsilon) &&
+        self.y.abs_diff_eq(&other.y, epsilon) &&
+        self.z.abs_diff_eq(&other.z, epsilon) &&
+        self.w.abs_diff_eq(&other.w, epsilon)
+     }
+}
+
 #[cfg(test)]
 mod tests {
 
     use super::*;
+    use approx::abs_diff_eq;
 
     #[test]
     fn tuple_with_w_equals_1_is_point() {
@@ -126,5 +180,85 @@ mod tests {
         let v2 = vector(5.0, 6.0, 7.0);
 
         assert_eq!(v1 - v2, vector(-2.0, -4.0, -6.0));
+    }
+
+    #[test]
+    fn subtracting_vector_from_zero_vector() {
+        let zero = vector(0.0, 0.0, 0.0);
+        let v = vector(1.0, -2.0, 3.0);
+        assert_eq!(zero - v, vector(-1.0, 2.0, -3.0));
+    }
+
+    #[test]
+    fn negating_a_tuple() {
+        let a = Tuple::new(1.0, -2.0, 3.0, -4.0);
+        assert_eq!(-a, Tuple::new(-1.0, 2.0, -3.0, 4.0))
+    }
+
+    #[test]
+    fn multiplying_tuple_by_scalar() {
+        let a = Tuple::new(1.0, -2.0, 3.0, -4.0);
+        assert_eq!(a * 3.5, Tuple::new(3.5, -7.0, 10.5, -14.0));
+    }
+
+    #[test]
+    fn multiplying_tuple_by_fraction() {
+        let a = Tuple::new(1.0, -2.0, 3.0, -4.0);
+        assert_eq!(a * 0.5, Tuple::new(0.5, -1.0, 1.5, -2.0));
+    }
+
+    #[test]
+    fn dividing_tuple_by_scalar() {
+        let a = Tuple::new(1.0, -2.0, 3.0, -4.0);
+        assert_eq!(a / 2.0, Tuple::new(0.5, -1.0, 1.5, -2.0));
+    }
+
+    #[test]
+    fn computing_magnitude_of_vector_1() {
+        let v = vector(1.0, 0.0, 0.0);
+        assert_eq!(v.magnitude(), 1.0);
+    }
+
+    #[test]
+    fn computing_magnitude_of_vector_2() {
+        let v = vector(0.0, 1.0, 0.0);
+        assert_eq!(v.magnitude(), 1.0);
+    }
+
+    #[test]
+    fn computing_magnitude_of_vector_3() {
+        let v = vector(0.0, 0.0, 1.0);
+        assert_eq!(v.magnitude(), 1.0);
+    }
+
+    #[test]
+    fn computing_magnitude_of_vector_4() {
+        let v = vector(1.0, 2.0, 3.0);
+        assert_eq!(v.magnitude(), 14.0_f32.sqrt());
+    }
+
+    #[test]
+    fn computing_magnitude_of_vector_5() {
+        let v = vector(-1.0, -2.0, -3.0);
+        assert_eq!(v.magnitude(), 14.0_f32.sqrt());
+    }
+
+    #[test]
+    fn normalizing_vector_1() {
+        let v = vector(4.0, 0.0, 0.0);
+        assert_eq!(v.normalize(), vector(1.0, 0.0, 0.0));
+    }
+
+    #[test]
+    fn normalizing_vector_2() {
+        let v = vector(1.0, 2.0, 3.0);
+        abs_diff_eq!(v.normalize(), vector(0.26726, 0.53452, 0.80178));
+    }
+
+    #[test]
+    fn magnitude_of_normalized_vector() {
+        let v = vector(1.0, 2.0, 3.0);
+        let norm = v.normalize();
+        abs_diff_eq!(norm.magnitude(), 1.0);
     }
 }
